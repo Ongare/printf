ca984da6a09a15ca337631009cc34f19bbabdbb9
Ongare <stephenongare2966@gmail.com>
2023-03-27 14:05:09 -0700

    printf

--- functions.c
+++ functions.c
@@ -1,9 +1,6 @@
 #include "main.h"
-
-/************************* PRINT CHAR *************************/
-
-/**
- * print_char - Prints a char
+/*
+ * * print_char - Prints a char
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
@@ -17,9 +14,9 @@ int print_char(va_list types, char buffer[],
 {
 	char c = va_arg(types, int);
 
-	return (handle_write_char(c, buffer, flags, width, precision, size));
+	return (handle_write_char(c, buffer, flags, width, precision, 
 }
-/************************* PRINT A STRING *************************/
+
 /**
  * print_string - Prints a string
  * @types: List a of arguments
@@ -74,9 +71,7 @@ int print_string(va_list types, char buffer[],
 
 	return (write(1, str, length));
 }
-/************************* PRINT PERCENT SIGN *************************/
-/**
- * print_percent - Prints a percent sign
+/** print_percent - Prints a percent sign
  * @types: Lista of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
@@ -97,9 +92,8 @@ int print_percent(va_list types, char buffer[],
 	return (write(1, "%%", 1));
 }
 
-/************************* PRINT INT *************************/
-/**
- * print_int - Print int
+
+/* Print int
  * @types: Lista of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
